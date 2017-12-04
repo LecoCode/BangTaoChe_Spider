c08d03d62ba7de4da518f74245869393fe796a96
lecoCore <510785657@qq.com>
2017-12-04 12:06:09 +0800

    结果处理服务

---
+++ bangtaoche-spider-Solver/src/main/java/com/bangtaoche/spider/solver/che_detailed/client.java
@@ -0,0 +1,11 @@
+package com.bangtaoche.spider.solver.che_detailed;
+
+/**
+ * @author: 李飞
+ * @Time: 17-12-4.
+ * @Emil: leco_li@126.com
+ * @GitHub: https://github.com/LecoCode
+ * @Function:
+ */
+public class client {
+}
